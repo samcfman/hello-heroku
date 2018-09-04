3e8869a2120f69acfdba966bdd041bbdb45c91d1
samcfman <sman@sman-ltm1.internal.salesforce.com>
2018-09-04 11:57:58 +0800

    modify index.js

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const server = http.createServer ((req, res)=>{
     res.end('Hello World\n');
 });
 
-server.listen(port, () => {
+server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
 });
