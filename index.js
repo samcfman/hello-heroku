e973f004bf24c5e129410855cc9689c021a7d51e
samcfman <36816578+samcfman@users.noreply.github.com>
2018-09-04 13:49:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer ((req, res)=>{
     res.statusCode = 200;
     res.setHeader ('Content-Type', 'text/plain');
-    res.end('Hello World !!! SAM-MAN\n');
+    res.end('Hello World !!! SAM-MAN ENABLE REVIEW APP\n');
 });
 
 server.listen(PORT, () => {
