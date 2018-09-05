b4e0dca215de8374ccc120847903ffb828eceaf4
samcfman <36816578+samcfman@users.noreply.github.com>
2018-09-05 12:50:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World !!! NEW FEATURE - ADD NEW ICON\n');
+  res.end('Hello World !!! NEW FEATURE - ADD NEW ICON 1\n');
 });
 
 server.listen(PORT, () => {
